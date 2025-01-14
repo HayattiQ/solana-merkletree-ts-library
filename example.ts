8afaf48ed89d17f3358ffbe26903b20de82fdf42
hayatti <gurujowa@gmail.com>
2025-01-14 13:32:33 +0900

    remove exmaple

--- example.ts
+++
@@ -1,7 +0,0 @@
-import { Claim, getLeavesFromClaims, getProofForAnchor } from "./index.ts";
-
-const claims:Claim[] = [
-    {address: "GzwXKoyqgj8wNu3vkV5awXyLBQnetRG2jNct2qVa5uJi", amount: 1234}
-]
-
-console.log(getLeavesFromClaims(claims));
